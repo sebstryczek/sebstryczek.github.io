b1b12473a374e6a8a599d7aa3c22aea44d675bb1
sebstryczek <sebastian@stryczek.pl>
2020-03-28 22:06:10 +0100

    Add three js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Layout from '../components/Layout';
 import Container from '../components/Layout/Container';
 import SEO from '../components/SEO';
-
+import { initPortfolio, destroyPortfolio } from '../three-apps/portfolio';
 
 const Index = ({ data }) => {
+  useEffect(() => {
+    console.log('?')
+    initPortfolio();
+    return destroyPortfolio;
+  }, []);
+
   return (
     <>
       <SEO title='hi!' />
